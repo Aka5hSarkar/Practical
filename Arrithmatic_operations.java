a9b2e09f260f4648fd029cf2f65706b6a94417b7
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-08-09 17:46:24 +0530

    Add files via upload

---
+++ Arrithmatic_operations.java
@@ -0,0 +1,15 @@
+class Arrithmatic_operations{
+public static void main(String []args){
+System.out.println("The two numbers are :");
+int a = 12;//initialize 1st local variable 
+int b = 6;//initialize 2nd local variable
+System.out.println(a + " " + b);
+int sum = a+b;// logic
+System.out.println("The sum is :" + sum);
+int substract = a-b;
+System.out.println("The substraction is :" + substract);
+int multiply = a*b;
+System.out.println("The multiplication is :" + multiply);
+int div = a/b;
+System.out.println("The quotient is :" + div);
+}}
