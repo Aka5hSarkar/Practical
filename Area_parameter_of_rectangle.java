707beda9c814ae4dba287b738a1c70ac6ba300a3
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-08-10 16:31:31 +0530

    Update Area_parameter_of_rectangle.java

--- Area_parameter_of_rectangle.java
+++ Area_parameter_of_rectangle.java
@@ -1,11 +1,9 @@
 class Area_parameter_of_rectangle{
 public static void main(String []args){
-System.out.print("The length is :");
+System.out.print("The length & width are :");
 int a = 5;//initialize the length
-System.out.println(a);
-System.out.print("The width is :");
 int c = 6;//initialize the width
-System.out.println(c);
+System.out.println(a + " and " + c + " respectively ");
 int area = a*c;
 System.out.println("The area of the rectangle is :" + area);
 int par = 2*(a+c);
