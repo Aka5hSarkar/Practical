1154c52ae66388be7a4268b8c5ed341c0f9b41c7
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-08-10 15:33:11 +0530

    Add files via upload

---
+++ Area_parameter_of_square.java
@@ -0,0 +1,12 @@
+class Area_parameter_of_square{
+public static void main(String []args){
+System.out.print("The side length is :");
+int a = 5;//initialize the side length
+System.out.println(a);
+int area = a*a;
+System.out.println("The area of the square is :" + area);
+int par = a*4;
+System.out.println("The parameter of the square is :" + par);
+}}
+
+
