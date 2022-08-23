3abe8fd7477163b38f5577ed5e07840c709b49d1
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-08-23 18:04:11 +0530

    Add files via upload

---
+++ InfinityFor.java
@@ -0,0 +1,12 @@
+package JavaClass;
+
+public class InfinityFor {
+
+	public static void main(String[] args) {
+		for(int i=1;;) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ NumPattern.java
@@ -0,0 +1,14 @@
+package JavaClass;
+
+public class NumPattern {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=9;i++) {
+			for (int j=i;j<=i;j++) {
+				System.out.print(j);
+			}System.out.println();
+		}
+
+	}
+
+}
