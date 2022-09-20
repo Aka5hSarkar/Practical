86cd28b3f169b5b04886ad842953a7d8c592e294
Aka5hSarkar <98515020+Aka5hSarkar@users.noreply.github.com>
2022-09-20 18:10:52 +0530

    Add files via upload

---
+++ StudentTest.java
@@ -0,0 +1,12 @@
+package JavaClass;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.setName("akash");
+		System.out.println(s.getName());
+
+	}
+
+}
